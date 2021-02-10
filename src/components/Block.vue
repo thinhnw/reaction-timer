7d557d635e45bf4874ac51c333431d0560c273b1
thinhnw <thinhnv0412@gmail.com>
2021-02-10 22:31:11 +0700

    Set delay to show block

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="block">
-      click me
-  </div>
+    <div class="block" v-if="showBlock"> 
+        click me
+    </div>
 </template>
 
 <script>
 export default {
-    props: [ 'delay' ]
+    props: [ 'delay' ],
+    data() {
+        return {
+            showBlock: false
+        }
+    },
+    mounted() {
+        console.log('component mounterd')
+        setTimeout(() => {
+            this.showBlock = true
+        }, this.delay)
+    }
 }
 </script>
 
